bcd29551eaa84ea705da86ff55c9a58e827cff22
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-18 22:36:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nikhilp-patterns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nikhilp-patterns)
---
+++ index.js
@@ -0,0 +1,31 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var row = 5,
+
+//pattern 1
+
+// for (var i = 1; i <= row; i++) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write('* ');
+//   }
+//   document.write('<br/>');
+// }
+
+//pattern 1 end
+
+
+//pattern 2
+
+// for (var i = 1; i <= row; i++) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write(j + " ");
+//   }
+//   document.write('<br/>');
+// }
+
+//pattern 2 end
