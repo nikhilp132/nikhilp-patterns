cff669e6c71acb758a0322744879d407fca77ff5
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-21 12:09:27 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,55 +6,56 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 var row = 5,
-  //pattern 1
+//pattern 1
 
-  // for (var i = 1; i <= row; i++) {
-  //   for (var j = 1; j <= i; j++) {
-  //     document.write('* ');
-  //   }
-  //   document.write('<br/>');
-  // }
+// for (var i = 1; i <= row; i++) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write('* ');
+//   }
+//   document.write('<br/>');
+// }
 
-  //pattern 1 end
+//pattern 1 end
 
-  //pattern 2
+//pattern 2
 
-  // for (var i = 1; i <= row; i++) {
-  //   for (var j = 1; j <= i; j++) {
-  //     document.write(j + " ");
-  //   }
-  //   document.write('<br/>');
-  // }
+// for (var i = 1; i <= row; i++) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write(j + " ");
+//   }
+//   document.write('<br/>');
+// }
 
-  //pattern 2 end
+//pattern 2 end
 
-  //pattern 3
+//pattern 3
 
-  // for (var i = 1; i <= row; i++) {
-  //   for (var j = row; j >=i; j--) {
-  //     document.write('*' + " ");
-  //   }
-  //   document.write('<br/>');
-  // }
+// for (var i = 1; i <= row; i++) {
+//   for (var j = row; j >=i; j--) {
+//     document.write('*' + " ");
+//   }
+//   document.write('<br/>');
+// }
 
-  //pattern 3 end
+//pattern 3 end
 
-  //pattern 4
+//pattern 4
 
-  // for (var i = 1; i <= row; i++) {
-  //   for (var j = 1; j <=row+i-1; j++) {
-  //     if(j < row-j){
-  //       document.write("#");
-  //     }else{
-  //       document.write('*' + " ");
-  //     }
-  //   }
-  //   document.write('<br/>');
-  // }
+// for (var i = 1; i <= row; i++) {
+//   for (var j = 1; j <=row+i-1; j++) {
+//     if(j < row-j){
+//       document.write("#");
+//     }else{
+//       document.write('*' + " ");
+//     }
+//   }
+//   document.write('<br/>');
+// }
 
-  //pattern 4 end
+//pattern 4 end
 
-  //pattern
+  
+//pattern
 
 //   row = 4;
 // var number = 1;
