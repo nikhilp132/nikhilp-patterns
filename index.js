92b3e7cc44addec62ad5ecf4d1d79432fcc30ecc
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-21 13:31:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,60 +6,59 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 var n = 5,
-//pattern 1
+  //pattern 1
 
-// for (var i = 1; i <= n; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write('* ');
-//   }
-//   document.write('<br/>');
-// }
+  // for (var i = 1; i <= n; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //     document.write('* ');
+  //   }
+  //   document.write('<br/>');
+  // }
 
-//pattern 1 end
+  //pattern 1 end
 
-//pattern 2
+  //pattern 2
 
-// for (var i = 1; i <= n; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write(j + " ");
-//   }
-//   document.write('<br/>');
-// }
+  // for (var i = 1; i <= n; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //     document.write(j + " ");
+  //   }
+  //   document.write('<br/>');
+  // }
 
-//pattern 2 end
+  //pattern 2 end
 
-//pattern 3
+  //pattern 3
 
-// for (var i = 1; i <= n; i++) {
-//   for (var j = n; j >=i; j--) {
-//     document.write('*' + " ");
-//   }
-//   document.write('<br/>');
-// }
+  // for (var i = 1; i <= n; i++) {
+  //   for (var j = n; j >=i; j--) {
+  //     document.write('*' + " ");
+  //   }
+  //   document.write('<br/>');
+  // }
 
-//pattern 3 end
+  //pattern 3 end
 
-//pattern 4
+  //pattern 4
 
-// for(var i = n; i >= 1; i--){
-//   for(var j = 1; j <= n; j++){
-    
-//   }
-// }
+  // for(var i = n; i >= 1; i--){
+  //   for(var j = 1; j <= n; j++){
 
-//pattern 4 end
+  //   }
+  // }
 
-  
-//pattern
+  //pattern 4 end
 
-//   n = 4;
-// var number = 1;
-// for (var i = 1; i <= n; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write(number + ' ');
-//     number++;
-//   }
-//   document.write('<br/>');
-// }
+  // pattern;
 
-//pattern
+  n = 4;
+var number = 1;
+for (var i = 1; i <= n; i++) {
+  for (var j = 1; j <= i; j++) {
+    document.write(number + ' ');
+    number++;
+  }
+  document.write('<br/>');
+}
+
+// pattern
