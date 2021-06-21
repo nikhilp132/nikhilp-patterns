fe490686fc5c2ee4fc498650318ccf76317e5096
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-21 18:43:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,55 +6,63 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 var n = 5,
-  //pattern 1
+//pattern 1
 
-  // for (var i = 1; i <= n; i++) {
-  //   for (var j = 1; j <= i; j++) {
-  //     document.write('* ');
-  //   }
-  //   document.write('<br/>');
-  // }
+// for (var i = 1; i <= n; i++) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write('* ');
+//   }
+//   document.write('<br/>');
+// }
 
-  //pattern 1 end
+//pattern 1 end
 
-  //pattern 2
 
-  // for (var i = 1; i <= n; i++) {
-  //   for (var j = 1; j <= i; j++) {
-  //     document.write(j + " ");
-  //   }
-  //   document.write('<br/>');
-  // }
 
-  //pattern 2 end
+//pattern 2
 
-  //pattern 3
+// for (var i = 1; i <= n; i++) {
+//   for (var j = 1; j <= i; j++) {
+//     document.write(j + " ");
+//   }
+//   document.write('<br/>');
+// }
 
-  // for (var i = 1; i <= n; i++) {
-  //   for (var j = n; j >=i; j--) {
-  //     document.write('*' + " ");
-  //   }
-  //   document.write('<br/>');
-  // }
+//pattern 2 end
 
-  //pattern 3 end
 
-  //pattern 4
 
-  for(var i = 1; i <= n; i++){
-    for(var j = 1; j <= 2*n-1; j++){
-      if(j > n-i && j < n + i){
-        document.write('* ');
-      }else{
-        document.write('&nbsp;&nbsp;');
-      }
-    }
-    document.write('<br/> ');
-  }
+//pattern 3
 
-  //pattern 4 end
+// for (var i = 1; i <= n; i++) {
+//   for (var j = n; j >=i; j--) {
+//     document.write('*' + " ");
+//   }
+//   document.write('<br/>');
+// }
 
-  // pattern;
+//pattern 3 end
+
+
+  
+//pattern 4
+
+// for(var i = 1; i <= n; i++){
+//   for(var j = 1; j <= 2*n-1; j++){
+//     if(j > n-i && j < n + i){
+//       document.write('*&nbsp;');
+//     }else{
+//       document.write('&nbsp;&nbsp;&nbsp;');
+//     }
+//   }
+//   document.write('<br/> ');
+// }
+
+//pattern 4 end
+
+
+
+// pattern 8
 
 //   n = 4;
 // var number = 1;
@@ -66,4 +74,4 @@ var n = 5,
 //   document.write('<br/>');
 // }
 
-// pattern
+// pattern8
