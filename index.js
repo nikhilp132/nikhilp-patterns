1eecaf8dec2a3b5d4599eaaa5e28a8a519818ec9
nikhilp132 <nikhilparadkar200@gmail.com>
2021-06-21 12:02:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,54 +6,64 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 var row = 5,
+  //pattern 1
 
-//pattern 1
+  // for (var i = 1; i <= row; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //     document.write('* ');
+  //   }
+  //   document.write('<br/>');
+  // }
 
+  //pattern 1 end
+
+  //pattern 2
+
+  // for (var i = 1; i <= row; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //     document.write(j + " ");
+  //   }
+  //   document.write('<br/>');
+  // }
+
+  //pattern 2 end
+
+  //pattern 3
+
+  // for (var i = 1; i <= row; i++) {
+  //   for (var j = row; j >=i; j--) {
+  //     document.write('*' + " ");
+  //   }
+  //   document.write('<br/>');
+  // }
+
+  //pattern 3 end
+
+  //pattern 4
+
+  // for (var i = 1; i <= row; i++) {
+  //   for (var j = 1; j <=row+i-1; j++) {
+  //     if(j < row-j){
+  //       document.write("#");
+  //     }else{
+  //       document.write('*' + " ");
+  //     }
+  //   }
+  //   document.write('<br/>');
+  // }
+
+  //pattern 4 end
+
+  //pattern
+
+//   row = 4;
+// var number = 1;
 // for (var i = 1; i <= row; i++) {
 //   for (var j = 1; j <= i; j++) {
-//     document.write('* ');
+//     document.write(number + ' ');
+//     number++;
 //   }
 //   document.write('<br/>');
 // }
 
-//pattern 1 end
-
-
-//pattern 2
-
-// for (var i = 1; i <= row; i++) {
-//   for (var j = 1; j <= i; j++) {
-//     document.write(j + " ");
-//   }
-//   document.write('<br/>');
-// }
-
-//pattern 2 end
-
-
-//pattern 3
-
-// for (var i = 1; i <= row; i++) {
-//   for (var j = row; j >=i; j--) {
-//     document.write('*' + " ");
-//   }
-//   document.write('<br/>');
-// }
-
-//pattern 3 end
-
-
-//pattern 4
-var row = 2;
-for (var i = 1; i <= row; i++) {
-  for (var j = 1; j <=row+i-1; j++) {
-    if(j < row-j){
-      document.write("#");
-    }else{
-      document.write('*' + " ");
-    }
-  }
-  document.write('<br/>');
-}
-
-//pattern 4 end
+//pattern
